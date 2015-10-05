d803e49129c0a095475e78e508b480d227ecb4da
Julian Shen <julianshen@gmail.com>
2015-10-05 15:22:46 +0800

    Initial Commit

---
+++ SwSelect/SwSelect.h
@@ -0,0 +1,19 @@
+//
+//  SwSelect.h
+//  SwSelect
+//
+//  Created by Julian Shen on 2015/10/5.
+//  Copyright © 2015年 cowbay.wtf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwSelect.
+FOUNDATION_EXPORT double SwSelectVersionNumber;
+
+//! Project version string for SwSelect.
+FOUNDATION_EXPORT const unsigned char SwSelectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwSelect/PublicHeader.h>
+
+
